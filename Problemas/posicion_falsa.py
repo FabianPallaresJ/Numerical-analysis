fb84da4f13711f628c0bb25aa8157f95379ad315
Fabián Pallares <fabianpallares23@gmail.com>
2019-08-04 21:26:46 -0500

    Método secante añadido

---
+++ Problemas/posicion_falsa.py
@@ -0,0 +1,44 @@
+from pylab import *
+import math
+
+
+def check(a, b):
+    if f(a) * f(b) < 0:
+        return True
+
+def biseccion(f, a, b, e):
+    count = 0
+    c = 0
+    while b - a >= e:
+        count += 1
+        c = (a + b) / 2
+        if f(c) == 0:
+            return c
+        else:
+            if f(a)  * f(c) > 0:
+                a = c
+            else:
+                b = c
+    print("Iteraciones: " + str(count))
+    return c
+
+def graph(a, b):
+    x = arange(0, 20, 0.1)
+    f = x * math.exp(x) - math.pi
+    plot(x,f)
+    grid(True)
+    show()
+
+#def f(x): return (x * math.exp(x)) - math.pi
+
+def f(x): return (math.exp(x) - math.pi * (x))
+
+if __name__ == "__main__":
+    a = float(input("Defina punto a del intervalo: "))
+    b = float(input("Defina punto b del intervalo: "))
+    e = float(input("Defina el error: "))
+
+    if(check(a, b)):
+        print(p_falsa(f, a , b, e))
+        print(f(p_falsa(f, a , b, e)))
+    #graph(a, b)
