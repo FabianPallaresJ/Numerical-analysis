8f4669da194a3034656bb7172b947c970dc0e244
Fabián Pallares <51183566+FabianPallaresJ@users.noreply.github.com>
2019-08-20 00:27:50 -0500

    Método de Müller

---
+++ Talleres/muller.py
@@ -0,0 +1,59 @@
+import math
+
+def check(a, b):
+    if f(a) * f(b) < 0:
+        return True
+
+def muller(f, x0, x1, e):
+    x2 = (x0 + x1) /2
+    h0 = 0
+    h1 = 0
+    d0 = 0
+    d1 = 0
+    a = 0
+    b = 0
+    c = 0
+    xaux = 0
+    count = 0
+    while count < e:
+        print("=== It " + str(count + 1) + " ===")
+        count += 1
+        h0 = x1 - x0
+        h1 = x2 - x1
+        print("h0: " + str(h0))
+        print("h1: " + str(h1))
+
+        d0 = (f(x1) - f(x0))/(x1 - x0)
+        d1 = (f(x2) - f(x1))/(x2 - x1)
+        print("d0: " + str(d0))
+        print("d1: " + str(d1))
+
+        a = (d1 - d0) / (h1-h0)
+        b = a * h1 + d1
+        c = f(x2)
+
+        if b < 0:
+            xaux = x2 + ((-2 * c) / (b - math.sqrt((b**2)-4*a*c)))
+        else:
+            xaux = x2 + ((-2 * c) / (b + math.sqrt((b**2)-4*a*c)))
+
+        x0 = x1
+        x1 = x2
+        x2 = xaux
+
+    print("Resultado raíz = " + str(xaux))
+    print("Iteraciones: " + str(count))
+
+#def f(x): return (x * math.exp(x)) - math.pi
+
+def f(x): return (math.exp(-x) - x)
+
+if __name__ == "__main__":
+    x0 = float(input("Defina punto x0 del intervalo: "))
+    x1 = float(input("Defina punto x1 del intervalo: "))
+    e = float(input("Defina el numero en iteraciones: "))
+
+    if(check(x0, x1)):
+        muller(f, x0, x1, e)
+    else:
+        print("No hay raíces en este intervalo")
